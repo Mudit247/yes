ebaf1af1c6de1eb25c06e61cd7b164f8ad2e27fd
mudit247365 <guptmudit2005@gmail.com>
2025-10-19 19:28:13 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to mudit .
+          Edit <code>src/App.js</code> and save to abhinav .
         </p>
         <a
           className="App-link"
